39d0013b6ecb6ecec44c078279210128c91565e6
Wayne H. Shephard <mail@iovchinnikov.ru>
2023-12-22 21:45:15 +0300

    minor const fix

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,13 +1,13 @@
 #include "../include/Sprite.hpp"
 
 float scs::Sprite::getLeft() const { return x - halfWidth; }
-void scs::Sprite::setLeft(float left) { x = left + halfWidth; }
+auto scs::Sprite::setLeft(const float left) -> void { x = left + halfWidth; }
 float scs::Sprite::getRight() const { return x + halfWidth; }
-void scs::Sprite::setRight(float right) { x = right - halfWidth; }
+void scs::Sprite::setRight(const float right) { x = right - halfWidth; }
 float scs::Sprite::getTop() const { return y - halfHeight; }
-void scs::Sprite::setTop(float top) { y = top + halfHeight; }
+void scs::Sprite::setTop(const float top) { y = top + halfHeight; }
 float scs::Sprite::getBottom() const { return y + halfHeight; }
-void scs::Sprite::setBottom(float bottom) { y = bottom - halfHeight; }
+void scs::Sprite::setBottom(const float bottom) { y = bottom - halfHeight; }
 
 float scs::Sprite::getWidth() const { return 2 * halfWidth; }
 float scs::Sprite::getHeight() const { return 2 * halfHeight; }
